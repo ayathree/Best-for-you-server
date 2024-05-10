975a17231f637f6d2378f367ad522fd139b0aae7
ayathree <nobanitaayathree333@gmail.com>
2024-05-11 00:08:30 +0600

    created add functionality of crud method

--- index.js
+++ index.js
@@ -32,14 +32,24 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const productsCollection = client.db('alterDB').collection('products')
+
+    // save a product data in database
+   app.post('/products',async(req,res)=>{
+    const productData = req.body;
+    console.log(productData)
+    
+    const result = await productsCollection.insertOne(productData)
+    res.send(result)
+ })
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
