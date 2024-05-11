1098e12270b75904e6d518e88a213c44c5f7efcd
ayathree <nobanitaayathree333@gmail.com>
2024-05-11 20:20:02 +0600

    added functionality for delete

--- index.js
+++ index.js
@@ -59,6 +59,13 @@ app.get('/product/:id', async(req,res)=>{
   res.send(result)
 
 })
+// delete a single product data from db
+app.delete('/product/:id',async(req,res)=>{
+  const id = req.params.id
+  const query = {_id: new ObjectId(id)}
+  const result = await productsCollection.deleteOne(query)
+      res.send(result)
+   })
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
